50f20de8bdd2e6554efb68c0f95ad572e1d57d99
Taha Boudouma <tahaboudouma@gmail.com>
2023-09-10 18:33:44 +0100

    'initial'

---
+++ src/components/Lander.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import '../style/global.css';
+
+const Lander = () => {
+    return (
+        <div className="lander">
+            <h1>GroundZero.ma</h1>
+            <p>We're dedicated to directly supporting local associations and volunteers, ensuring they have the funds to act promptly on the ground. Donations are transparently directed to verified groups. Soon, our interactive map will provide real-time visuals of operations as they unfold.</p>
+        </div>
+    );
+}
+
+export default Lander;
